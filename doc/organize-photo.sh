25903336f3c39cbb41ef004103ff1a6e962f02a7
Jon McLennan <cabuki@gmail.com>
2016-07-14 22:50:48 +0200

    Getting directory of file to pass full path

--- doc/organize-photo.sh
+++ doc/organize-photo.sh
@@ -4,11 +4,23 @@
 export targetroot=/backup/pictures
 export EYEFI_LOG="/var/log/iii/upload.log"
 
+
+SOURCE="${BASH_SOURCE[0]}"
+
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+
 while [[ $# -gt 0 ]]; do
 
     export EYEFI_UPLOADED="${1}"
 
-    source ./on-upload-photo.bash
+    source "$DIR"/on-upload-photo.bash
 
     shift
 
